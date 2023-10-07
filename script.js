768b8609f55a38822e6444be5ea15408421eedf5
NeuraiProject <122438095+NeuraiProject@users.noreply.github.com>
2023-10-07 19:21:15 +0200

    First version

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const progressBar = document.querySelector(".progress-bar");
+    const percentageElem = document.getElementById("percentage");
+    const donatedAmountElem = document.getElementById("donatedAmount");
+    const copyButton = document.getElementById("copy-button");
+    const addressInput = document.getElementById("address");
+  
+    // Donation Goal
+    const donationGoal = 60000000; 
+  
+    // Percentage of donations
+    function calculateDonatedPercentage(donated, goal) {
+      return (donated / goal) * 100;
+    }
+  
+    // Progress Bar
+    function updateProgressBar(donatedAmount) {
+      const donatedPercentage = calculateDonatedPercentage(donatedAmount, donationGoal);
+      if (donatedPercentage <= 100) {
+        progressBar.style.width = `${donatedPercentage}%`;
+        percentageElem.textContent = `${Math.floor(donatedPercentage)}% - `;
+        donatedAmountElem.textContent = `${donatedAmount} XNA`;
+      }
+    }
+  
+    // Read info from donate.txt
+    fetch('https://donation.neurai.org/donate.txt')
+      .then(response => response.text())
+      .then(data => {
+        const donatedAmount = parseFloat(data.trim());
+        updateProgressBar(donatedAmount);
+      })
+      .catch(error => {
+        console.error('Error in file "donate.txt":', error);
+      });
+  
+    // Copy address
+  });
+  function copyAddress() {
+    let addressInput = document.getElementById('address-input');
+    addressInput.select();
+    addressInput.setSelectionRange(0, 99999); /* For mobile devices */
+    document.execCommand('copy');
+}
+
+    // Reload web every 120sg
+setTimeout(function(){
+    window.location.reload();
+ }, 120000);
+  
